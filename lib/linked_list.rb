b60fe513294becbdd6684a9bc3911248c3d6a7f0
Ana Benites <anitabenites2014@gmail.com>
2019-04-03 16:26:03 +0200

    Add implementation and tests for the remove method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'pry-byebug'
 
 class LinkedList
   attr_accessor :head, :tail
@@ -7,11 +8,34 @@ class LinkedList
   end
 
   def add_first(node)
-    temp = @head
-    @head = node
-    @head.next = temp
-
+    node.next = nil
+    @head = @tail = node if @counter.zero?
+    if @counter >= 1
+      @tail.next = node
+      #tail becomes node
+      @tail = node
+    end
     @counter += 1
-    @tail = @head if @counter == 1
   end
+
+  def remove_first
+    return if @counter == 0
+    if @counter == 1
+      @head = @tail = nil
+    elsif @counter > 1
+      @head = @head.next
+    end
+    @counter -= 1
+  end
+
+  def remove_last
+    temp = @head
+    while temp.next != @tail do
+      temp = temp.next
+    end
+    temp.next = nil
+    @tail = temp
+    @counter -= 1
+  end
+
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -48,12 +48,72 @@ RSpec.describe LinkedList do
       end
 
       it 'adds a node to the beginning of the list' do
-        expect(list.head).to eql(node_three)
+        expect(list.head).to eql(node_one)
       end
 
       it 'points head to the next node' do
         expect(list.head.next).to eql(node_two)
       end
+    end
+  end
+
+  describe '#remove_first' do
+
+    context 'when the list is empty' do
+      it 'does not raise an error'do
+        expect { list.remove_first }.not_to raise_error
+      end
+    end
+
+    context 'with one node' do
+      before do
+        list.add_first(node_one)
+      end
+      it 'sets the node to nil' do
+        list.remove_first
+        expect(list.head).to be_nil
+        expect(list.tail).to be_nil
+      end
+    end
+
+    context 'with more than one node' do
+      before do
+        list.add_first(node_one)
+        list.add_first(node_two)
+        list.add_first(node_three)
+      end
+      it 'removes a node at the beginning of the list' do
+        list.remove_first
+        expect(list.head).to eq(node_two)
+      end
+    end
+  end
+
+  describe '#remove_last' do
+    context 'with more than one node' do
+      before do
+        list.add_first(node_one)
+        list.add_first(node_two)
+        list.add_first(node_three)
+      end
+      it 'removes a node at the end of the list' do
+        list.remove_last
+        expect(list.tail).to eq(node_two)
+      end
+    end
+  end
+
+  describe 'building the list' do
+    before do
+      list.add_first(node_one)
+      list.add_first(node_two)
+      list.add_first(node_three)
+    end
+    it 'set the correct head of the list' do
+      expect(list.head).to eql(node_one)
+    end
+    it 'set the correct tail of the list' do
+      expect(list.tail).to eql(node_three)
     end
   end
 end
