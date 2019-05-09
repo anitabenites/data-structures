0251fb6938f5beecc710449f1a0f2ed7ac22ed81
Ana Benites <anitabenites2014@gmail.com>
2019-05-09 17:26:12 +0200

    Add the find method for bst and working on the remove method - WIP

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -9,8 +9,47 @@ class BinarySearchTree
 
   def insert(value)
     node = BinarySearchTreeNode.new(value:value)
-    # self.root_node = node if root_node.nil?
-    @root_node = node if root_node.nil?
-    # root_node = node if root_node.nil?
+    return @root_node = node if root_node.nil?
+    temp = @root_node
+
+    while true #TO DO
+      if node.value < temp.value
+        if temp.left == nil
+          temp.left = node
+          return
+        else
+          temp = temp.left
+        end
+      else
+        if temp.right == nil
+          temp.right = node
+          return
+        else
+          temp = temp.right
+        end
+      end
+    end
   end
+
+  def find(value)
+    find_node(root_node, value)
+  end
+
+  def find_node(node, value)
+    return nil unless node
+    return node if node.value == value
+    if value < node.value
+      find_node(node.left, value)
+    else
+      find_node(node.right, value)
+    end
+  end
+
+  def remove(value)
+    remove_node(root_node, value)
+  end
+
+  # def remove_node(value)
+  #
+  # end
 end
--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,29 +1,96 @@
 
 RSpec.describe BinarySearchTree do
   let(:tree) { described_class.new }
+
   describe '#insert' do
 
     context 'with no node' do
       it 'insert a new node to the tree' do
-        tree.insert(1)
-        expect(tree.root_node.value).to eql(1)
+        tree.insert(4)
+        expect(tree.root_node.value).to eql(4)
       end
     end
 
-    # context 'with one node' do
-    #   it ''
-    # end
-    #
-    # context 'with 2 nodes' do
-    #   it ''
-    # end
-    #
-    # context 'with 3 nodes' do
-    #   it ''
-    # end
-    #
-    # context 'with 4 nodes' do
-    #
-    # end
+    context 'with one node' do
+      before do
+        tree.insert(4)
+      end
+      it 'insert a new node to the left of the root' do
+        tree.insert(2)
+        expect(tree.root_node.left.value).to eql(2)
+      end
+      it 'insert a new node to the right side of the root' do
+        tree.insert(6)
+        expect(tree.root_node.right.value).to eql(6)
+      end
+    end
+
+    context 'with 7 nodes' do
+      before do
+        tree.insert(4)
+        tree.insert(2)
+        tree.insert(6)
+      end
+      it 'insert a new node to the left of the parent node' do
+        tree.insert(1)
+        expect(tree.root_node.left.left.value).to eql(1)
+      end
+      it 'insert a new node to the right of the parent node' do
+        tree.insert(3)
+        expect(tree.root_node.left.right.value).to eql(3)
+      end
+
+      it 'insert a new node to the right of the parent node' do
+        tree.insert(7)
+        expect(tree.root_node.right.right.value).to eql(7)
+      end
+
+      it 'insert a new node to the left of the parent node' do
+        tree.insert(5)
+        expect(tree.root_node.right.left.value).to eql(5)
+      end
+    end
+    #TO DO: when we have the same value!
+
+    context 'with 3 nodes when the node have the same value' do
+      before do
+        tree.insert(4)
+        tree.insert(2)
+        tree.insert(6)
+      end
+      #it 'inserts a node that already exits ' do
+      #  tree.insert(2)
+      #  expect(tree.root_node.left.value).to exist
+      #end
+    end
+  end
+
+  describe '#find' do
+    context 'with 3 nodes' do
+      let(:values) { [4,2,6] }
+      before do
+        values.each { |val| tree.insert(val) }
+      end
+      it 'finds the nodes' do
+        values.each do |val|
+          expect(tree.find(val).value).to eql(val)
+        end
+      end
+       it 'returns nil if the node can not be found' do
+         expect(tree.find(9)).to be_nil
+       end
+    end
+  end
+
+  describe '#remove' do
+    context 'with 3 nodes' do
+      let(:values) { [4, 2, 6] }
+      before do
+        values.each { |val| tree.insert(val) }
+      end
+      it 'removes a node' do
+        expect(tree.remove(2)).to be_nil
+      end
+    end
   end
 end
